25c1da5c2996ab2803f11a8428ed70255928f26d
Андрей <mihailov@runica.pro>
2024-06-27 12:19:05 +0300

    добавил сендер и задачу селери

---
+++ worker/sender/tasks.py
@@ -0,0 +1,21 @@
+import json
+
+from celery import Celery
+from .message_sender import SendMessageManager
+
+
+def init_celery() -> Celery:
+    redis_url = "redis://redis"
+    return Celery("tasks", broker=redis_url, backend=redis_url)
+
+
+app = init_celery()
+
+
+@app.task
+def send(body) -> None:
+    body_decode = body.decode("utf-8")
+    message = json.loads(body_decode)
+
+    manager = SendMessageManager()
+    manager.send(message)
